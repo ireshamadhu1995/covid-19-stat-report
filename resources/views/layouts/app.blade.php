1775429b61616ff296590bfa14dd261c97a65f12
iresha1995 <ireshamd196@gmail.com>
2022-07-29 04:32:05 +0530

    add side redirection

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,6 +39,11 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
+                      
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ url('') }}">{{ __('Dashboard') }}</a>
+                                </li>
+                         
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
